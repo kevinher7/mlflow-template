2b7c7953d22940e23caa1a88d551a1b3d2e7e954
Kevin Hernandez <kevinhernem@gmail.com>
2025-11-28 01:49:21 +0900

    feat: use poetry package mode + init script

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Enter project name (snake_case): " name
+
+# Rename folder
+git mv src/project_name "src/$name"
+
+sed -i '' "s/project_name/$name/g" pyproject.toml
+sed -i '' "s/project_name/${name//_/-}/g" pyproject.toml
+
+git commit -m "chore: initialize project"
+
+echo "Project initialized as: $name"
+echo "Run 'poetry install' to complete setup"
