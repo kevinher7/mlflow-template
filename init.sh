5654d8e154a17d41d0da182b7220adc19844e043
Kevin Hernandez <kevinhernem@gmail.com>
2025-11-28 16:35:45 +0900

    feat: create new git history in init script

--- init.sh
+++ init.sh
@@ -7,8 +7,9 @@ git mv src/project_name "src/$name"
 sed -i '' "s/project_name/$name/g" pyproject.toml
 sed -i '' "s/project_name/${name//_/-}/g" pyproject.toml
 
+rm -rf .git
+git add .
 git commit -m "chore: initialize project"
-git remove origin
 
 echo "Project initialized as: $name"
 echo "Run 'poetry install' to complete setup"
