253aefc29df2001817f6cd3e35759d98d79f8eed
Kevin Hernandez <kevinhernem@gmail.com>
2025-12-05 01:13:16 +0900

    fix: add git init after removing .git

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sed -i '' "s/project_name/$name/g" pyproject.toml
 sed -i '' "s/project_name/${name//_/-}/g" pyproject.toml
 
 rm -rf .git
+git init
 git add .
 git commit -m "chore: initialize project"
 
